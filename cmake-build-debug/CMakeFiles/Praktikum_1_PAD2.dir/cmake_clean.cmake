641ff5944d477738a7d90ad269585faf1ffe5189
Goennjamin <leon.warren78@gmail.com>
2021-10-26 13:18:29 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Praktikum_1_PAD2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Praktikum_1_PAD2.dir/main.cpp.obj"
+  "Praktikum_1_PAD2.exe"
+  "Praktikum_1_PAD2.exe.manifest"
+  "Praktikum_1_PAD2.pdb"
+  "libPraktikum_1_PAD2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Praktikum_1_PAD2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
